6caf9549315d5bf3fb3f231a5c7da01d44f5de21
rcbcc25-sketch <rcbcc25@gmail.com>
2025-11-20 09:43:53 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8270081020:AAFSEWE4wkSmS38JqAzaJNid-b8c
 
 CREDIT = os.environ.get("CREDIT", "Mr CHOUHA🇳")
 # MongoDB Configuration
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "CHOCHAN00")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "DevThanos")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://CHOCHAN00:<CHOCHAN00>@cluster0.kcx9pkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Add your own atlas db
 MONGO_URL = DATABASE_URL  # For auth system
 
