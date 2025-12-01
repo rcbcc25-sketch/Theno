8b5f853005edc71bf729b8f82cadac0543f5fceb
rcbcc25-sketch <rcbcc25@gmail.com>
2025-12-01 15:40:54 +0530

    Change default thumbnail URL in vars.py
    Updated default thumbnail URL in environment variable.

--- vars.py
+++ vars.py
@@ -19,7 +19,7 @@ ADMINS = [int(x) for x in os.environ.get("ADMINS", "8050673236").split()]  # Def
 # Channel Configuration
 PREMIUM_CHANNEL = "https://t.me/bshwbhry"
 # Thumbnail Configuration
-THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "https://sensitive-orange-2ndgmnoedd.edgeone.app/IMG_20250727_093946_936.jpg").split())) # Image Link For Default Thumbnail 
+THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "https://ibb.co/SwgPNHgT").split())) # Image Link For Default Thumbnail 
 
 # Web Server Configuration
 WEB_SERVER = os.environ.get("WEB_SERVER", "False").lower() == "true"
